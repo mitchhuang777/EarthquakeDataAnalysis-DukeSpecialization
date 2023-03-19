63e364c82ef760f03b9df1b0e94c4c1347124f15
Mitch <mitchhuang777@gmail.com>
2023-03-20 00:19:14 +0800

    depth filter

---
+++ DepthFilter.java
@@ -0,0 +1,17 @@
+public class DepthFilter implements Filter {
+    private double minDepth;
+    private double maxDepth;
+
+    public DepthFilter(double min, double max) {
+        minDepth = min;
+        maxDepth = max;
+    }
+
+    public boolean satisfies(QuakeEntry qe) {
+        return (qe.getDepth() >= minDepth && qe.getDepth() <= maxDepth);
+    }
+
+    public String getName() {
+        return "Depth";
+    }
+}
